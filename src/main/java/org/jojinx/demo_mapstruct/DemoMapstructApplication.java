1130757fb48665f4c39468f2183e6c1117733562
khoa <128579628+vakhoa4875@users.noreply.github.com>
2024-08-10 13:29:23 +0700

    First commit

---
+++ src/main/java/org/jojinx/demo_mapstruct/DemoMapstructApplication.java
@@ -0,0 +1,13 @@
+package org.jojinx.demo_mapstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMapstructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMapstructApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jojinx/demo_mapstruct/DemoMapstructApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jojinx.demo_mapstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMapstructApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
